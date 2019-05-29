73e28ccf555e0579d65d93b450cda10eabd834cf
rohit23ahuja <rohit23ahuja@gmail.com>
2019-05-29 21:38:09 +0530

    Add files via upload

--- concurrency/src/main/java/com/learn/concurrency/producerconsumer/ProducerConsumer.java
+++ concurrency/src/main/java/com/learn/concurrency/producerconsumer/ProducerConsumer.java
@@ -3,32 +3,52 @@ package com.learn.concurrency.producerconsumer;
 public class ProducerConsumer {
 	private static int count;
 	private static int[] buffer;
-	
-	public static boolean isEmpty(int[] buffer){
-		return count==0;
+	private static Object lock = new Object();
+
+	public static boolean isEmpty(int[] buffer) {
+		return count == 0;
 	}
-	
-	public static boolean isFull(int[] buffer){
-		return count==buffer.length;
+
+	public static boolean isFull(int[] buffer) {
+		return count == buffer.length;
 	}
-	
+
 	static class Producer {
-		public void produce(){
-			while(isFull(buffer)){}
-			buffer[count++]=1;
+		public void produce() {
+			synchronized (lock) {
+				if (isFull(buffer)) {
+					try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				buffer[count++] = 1;
+				lock.notifyAll();
+			}
 		}
 	}
-	
+
 	static class Consumer {
-		public void consume(){
-			while(isEmpty(buffer)){}
-			buffer[--count]=0;
+		public void consume() {
+			synchronized (lock) {
+				while (isEmpty(buffer)) {
+					try {
+						lock.wait(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				buffer[--count] = 0;
+				lock.notifyAll();
+			}
+
 		}
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		buffer = new int[10];
-		count=0;
+		count = 0;
 		Producer p = new Producer();
 		Runnable r1 = () -> {
 			for (int i = 0; i < 50; i++) {
@@ -36,24 +56,24 @@ public class ProducerConsumer {
 			}
 			System.out.println("Done producing");
 		};
-		
+
 		Consumer c = new Consumer();
 		Runnable r2 = () -> {
-			for (int i = 0; i < 50; i++) {
+			for (int i = 0; i < 52; i++) {
 				c.consume();
 			}
 			System.out.println("Done consuming");
 		};
-		
+
 		Thread t1 = new Thread(r1);
 		Thread t2 = new Thread(r2);
-		
+
 		t1.start();
 		t2.start();
-		
+
 		t1.join();
 		t2.join();
-		
-		System.out.println("size of buffer "+count);
+
+		System.out.println("size of buffer " + count);
 	}
 }
